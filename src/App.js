c31f91c75f023f8ef8b0288e078bab0c062755c1
Tyler Auer <Tyler.Auer@gmail.com>
2020-06-27 10:09:39 -0700

    set up responsive layout for plane and grids

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  // null while not on the plane
+  const [apple, setApple] = useState(null);
+
   return (
     <div className="App">
       <header className="header">
         <h2 className="header__title">Fruit</h2>
         <div className="header__subtitle">
-          An aggregator of people's feelings about fruit inspired by XKCD
+          An aggregator of people's feelings about fruit. Inspired by XKCD
         </div>
       </header>
-      <main>
+      <main className="app">
         <div className="app__unranked-fruit-holder"></div>
-        <div className="app__plane"></div>
-        <div className="app__submit"></div>
+        <div className="app__plane">
+          <div className="app__axis">DRAG ME!</div>
+        </div>
+        <button className="app__submit">Submit</button>
       </main>
       <footer className="footer">
         <div className="footer__help-btn">Help</div>
@@ -21,6 +26,6 @@ function App() {
       </footer>
     </div>
   );
-}
+};
 
 export default App;
