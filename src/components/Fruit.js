6b4a977f4fcbd1c20859a7a170dab50a3023e624
Tyler Auer <Tyler.Auer@gmail.com>
2020-07-30 14:43:42 -0700

    apply style when dragged off graph

--- src/components/Fruit.js
+++ src/components/Fruit.js
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Draggable from 'react-draggable';
 import './Fruit.scss';
 
 const Fruit = ({ name, ratings, setRatings, scale }) => {
   const nodeRef = useRef(null);
+  const [isOnGraph, setIsOnGraph] = useState(true);
   const src = require(`../img/${name}.png`);
   const calculatedPostion = {
     // Turns 0 to 100 scale into
@@ -19,6 +20,16 @@ const Fruit = ({ name, ratings, setRatings, scale }) => {
   const onDrag = (e, position) => {
     e.preventDefault();
     e.stopPropagation();
+
+    const newX = position.x + scale.imgSize / 2;
+    const newY = position.y + scale.imgSize / 2;
+
+    // Set rating to null if dragged off the grid
+    if (newX < 0 || newX > scale.width || newY < 0 || newY > scale.height) {
+      setIsOnGraph(false);
+    } else {
+      setIsOnGraph(true);
+    }
   };
 
   const onStop = (e, position) => {
@@ -35,7 +46,6 @@ const Fruit = ({ name, ratings, setRatings, scale }) => {
         [name]: null,
       });
     } else {
-      // ELSE:
       const newXRating = (newX * 100) / scale.width;
       const newYRating = 100 - (newY * 100) / scale.height;
       //// set state in Matrix component
@@ -61,7 +71,9 @@ const Fruit = ({ name, ratings, setRatings, scale }) => {
         <img
           alt={name}
           src={src}
-          className={`fruit__img fruit__img--${name}`}
+          className={`fruit__img fruit__img--${name} ${
+            isOnGraph ? '' : 'fruit__img--off-graph'
+          }`}
           style={{
             height: scale.imgSize,
             width: scale.imgSize,
