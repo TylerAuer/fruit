8b36ad62bc872a3b7f0f4ef967a6456d13b8195c
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-05 08:25:12 -0700

    round x, y info to tenths place when submited

--- src/hooks/useManageUserRatings.js
+++ src/hooks/useManageUserRatings.js
@@ -5,12 +5,27 @@ const useManageUserRatings = () => {
   const [ratings, setRatings] = useState(fruitList);
 
   const submitRatings = () => {
+    const roundToTenths = (float) => {
+      return Math.round(float * 10) / 10;
+    };
+
+    const roundedRatings = { ...ratings };
+
+    // round data to one decimal place
+    Object.keys(roundedRatings).forEach((fruit) => {
+      // Check if not null
+      if (roundedRatings[fruit]) {
+        roundedRatings[fruit].x = roundToTenths(roundedRatings[fruit].x);
+        roundedRatings[fruit].y = roundToTenths(roundedRatings[fruit].y);
+      }
+    });
+
     fetch('/submit', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(ratings),
+      body: JSON.stringify(roundedRatings),
     });
   };
 
