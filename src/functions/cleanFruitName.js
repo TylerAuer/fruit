4ec1a0507a49d988ab26056e896ee490006afbd4
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-10 15:38:32 -0700

    extract cleanFruitName function

---
+++ src/functions/cleanFruitName.js
@@ -0,0 +1,6 @@
+const cleanFruitName = (fruitName) => {
+  return fruitName
+    .split('_') // Convert into array
+    .map((word) => word[0].toUpperCase() + word.slice(1)) // capitalize first letter
+    .join(' '); // Convert into string
+};
