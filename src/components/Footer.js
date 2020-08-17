e5b2c32bd0ff040dd756663281425e4b81892d86
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-17 10:18:29 -0700

    add modal for XKCD comic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import xkcdImg from '../img/xkcd388.png';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 import Button from './Button';
 import './Bottom.scss';
 
 const Footer = ({ submitRatings, showAggregate, setShowAggregate }) => {
+  const [showXKCDModal, setShowXKCDModal] = useState(false);
   const onClickOfSubmitRatings = () => {
     return showAggregate ? null : submitRatings();
   };
@@ -23,20 +27,45 @@ const Footer = ({ submitRatings, showAggregate, setShowAggregate }) => {
       </div>
       <header>
         <h1 className="bottom__title">Fruit Matrix</h1>
-        {/* <div className="bottom__subtitle">
-          An aggregator of people's feelings about fruit. Inspired by XKCD.
-        </div> */}
       </header>
       <div className="bottom__link-container">
         <Link to="/about" className="bottom__link">
           About
         </Link>
+
+        <button className="bottom__link" onClick={() => setShowXKCDModal(true)}>
+          Inspiration
+        </button>
+        <Modal
+          open={showXKCDModal}
+          onClose={() => setShowXKCDModal(false)}
+          center
+        >
+          <h2 className="modal__title">Inspired by XKCD</h2>
+          <img
+            src={xkcdImg}
+            className="modal__img"
+            alt="The comic that inspired this site"
+          />
+          <div className="modal__body">
+            <p>
+              This site was inspired by this post from Randall Munroe's webcomic{' '}
+              <a href="http://xkcd.com/">XKCD</a>. He's written{' '}
+              <a href="https://www.amazon.com/Randall-Munroe/e/B004H66444">
+                some great books
+              </a>{' '}
+              you should check out!
+            </p>
+            <p>
+              You can read more about the influences for this site on the{' '}
+              <Link to="/about">About Page</Link>.
+            </p>
+          </div>
+        </Modal>
+
         <Link to="/data" className="bottom__link">
           Data
         </Link>
-        <a className="bottom__link" href="https://xkcd.com/">
-          XKCD
-        </a>
       </div>
     </div>
   );
