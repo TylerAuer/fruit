0b34b20c5d03f4bee10d6356b9ac1b66fe67c587
Tyler Auer <Tyler.Auer@gmail.com>
2020-09-03 12:26:41 -0700

    add correlation values to chart

--- src/d3/drawEasyCor.js
+++ src/d3/drawEasyCor.js
@@ -7,7 +7,7 @@ const drawEasyCor = () => {
   const height = 760 - 3 * margin;
 
   // append the svg object to the body of the page
-  var svg = d3
+  const svg = d3
     .select('#easy-cor-d3')
     .append('svg')
     .attr('preserveAspectRatio', 'xMinYMin meet')
@@ -30,6 +30,30 @@ const drawEasyCor = () => {
       .range(['rgb(255, 255, 255)', 'rgb(255, 0, 183)'])
       .interpolate(d3.interpolateRgb);
 
+    // Add fruit icons to x-axis
+    svg
+      .selectAll('svgs')
+      .data(Object.keys(data))
+      .enter()
+      .append('svg:image')
+      .attr('xlink:href', (d) => require(`../img/${d}.min.svg`))
+      .attr('width', xScale.bandwidth() * 0.8) // icons are 80% of square size
+      .attr('height', xScale.bandwidth() * 0.8)
+      .attr('y', -1 * xScale.bandwidth()) // Move one square's height above grid
+      .attr('x', (d) => xScale(d) + xScale.bandwidth() * 0.1); // center icon
+
+    // Add fruit icons to Y-axis
+    svg
+      .selectAll('svgs')
+      .data(Object.keys(data))
+      .enter()
+      .append('svg:image')
+      .attr('xlink:href', (d) => require(`../img/${d}.min.svg`))
+      .attr('width', yScale.bandwidth() * 0.8) // icons are 80% of square size
+      .attr('height', yScale.bandwidth() * 0.8)
+      .attr('x', -1 * yScale.bandwidth()) // Move one square's height above grid
+      .attr('y', (d) => yScale(d) + yScale.bandwidth() * 0.1); // center icon
+
     for (let fruitRow in data) {
       // Add squares
       const rowData = d3.entries(data[fruitRow].x);
@@ -47,29 +71,21 @@ const drawEasyCor = () => {
         .attr('width', xScale.bandwidth())
         .attr('height', yScale.bandwidth());
 
-      // Add fruit icons to x-axis
+      // Add text
       svg
-        .selectAll('svgs')
+        .selectAll(`${fruitRow}Row`)
         .data(rowData)
         .enter()
-        .append('svg:image')
-        .attr('xlink:href', (d) => require(`../img/${d.key}.min.svg`))
-        .attr('width', xScale.bandwidth() * 0.8) // icons are 80% of square size
-        .attr('height', xScale.bandwidth() * 0.8)
-        .attr('y', -1 * xScale.bandwidth()) // Move one square's height above grid
-        .attr('x', (d) => xScale(d.key) + xScale.bandwidth() * 0.1); // center icon
-
-      // Add fruit icons to Y-axis
-      svg
-        .selectAll('svgs')
-        .data(rowData)
-        .enter()
-        .append('svg:image')
-        .attr('xlink:href', (d) => require(`../img/${d.key}.min.svg`))
-        .attr('width', yScale.bandwidth() * 0.8) // icons are 80% of square size
-        .attr('height', yScale.bandwidth() * 0.8)
-        .attr('x', -1 * yScale.bandwidth()) // Move one square's height above grid
-        .attr('y', (d) => yScale(d.key) + yScale.bandwidth() * 0.1); // center icon
+        .append('text')
+        .attr('color', 'black')
+        .attr('x', (d) => xScale(d.key))
+        .attr('y', yScale(fruitRow))
+        .attr('dx', xScale.bandwidth() / 2)
+        .attr('dy', yScale.bandwidth() / 2)
+        .attr('font-size', '14px')
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'middle')
+        .text((d) => Math.round(d.value * 100) / 100);
     }
   });
 };
