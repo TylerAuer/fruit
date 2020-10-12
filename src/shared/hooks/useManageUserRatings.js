706cb399c833183f4d1dda6a342dc065b0ec6958
Tyler Auer <Tyler.Auer@gmail.com>
2020-10-12 15:58:30 -0700

    redesign returning user modal

--- src/shared/hooks/useManageUserRatings.js
+++ src/shared/hooks/useManageUserRatings.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import fruitList from '../../shared/Fruit.json';
-import toaster from 'toasted-notes';
-import 'toasted-notes/src/styles.css';
-import '../components/Toasts/Toasts.scss';
 
 const useManageUserRatings = () => {
   const [ratings, setRatings] = useState(fruitList);
+  const [foundPreviousRatings, setFoundPreviousRatings] = useState(false);
 
   // When the app first loads, see if the user's active session matches
   // the session IDs in the Ratings table of the database. If so, populate the
@@ -14,23 +12,12 @@ const useManageUserRatings = () => {
     fetch('/previous-ratings').then((res) => {
       if (res.status === 200) {
         res.json().then((ratings) => setRatings(ratings));
-        toaster.notify(
-          <div className="toast__msg">
-            <p>
-              <b>Welcome back! We found your previous ratings!</b>
-            </p>
-            <p>
-              Don't like your choices? If you submit changes, we'll update the
-              ratings.
-            </p>
-          </div>,
-          { duration: 6000 }
-        );
+        setFoundPreviousRatings(true);
       }
     });
   }, []);
 
-  return { ratings, setRatings };
+  return { ratings, setRatings, foundPreviousRatings };
 };
 
 export default useManageUserRatings;
