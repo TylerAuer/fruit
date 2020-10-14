b68f33b4a10be4ffe257ee08fdf74cd1be07d095
Tyler Auer <Tyler.Auer@gmail.com>
2020-10-14 09:47:17 -0700

    add tests for jump links in /data and <h1> link

--- cypress/integration/site_navigation.js
+++ cypress/integration/site_navigation.js
@@ -14,6 +14,20 @@ describe('All pages load', () => {
   });
 });
 
+describe('<h1> in Header navigates to /', () => {
+  it('from /data', () => {
+    cy.visit('/data');
+    cy.get('h1').click();
+    cy.url().should('include', '#/');
+  });
+
+  it('from /about', () => {
+    cy.visit('/about');
+    cy.get('h1').click();
+    cy.location('hash').should('eq', '#/');
+  });
+});
+
 describe('"Back to Matrix" btn navigates to /', () => {
   it('from /data', () => {
     cy.visit('/data');
@@ -50,5 +64,31 @@ describe('Links and buttons in footer of /', () => {
   it('"Inspiration" opens xkcd Modal', () => {
     cy.get('.bottom__link').contains('Inspiration').click();
     cy.get('.react-responsive-modal-overlay');
+  });
+});
+
+describe('Jump links in /data', () => {
+  beforeEach(() => {
+    cy.visit('/data');
+  });
+
+  it('Rating Frequencies', () => {
+    cy.get('.nav__link').contains('Rating Frequencies').click();
+    cy.location('hash').should('eq', '#/data/#frequencies');
+  });
+
+  it('Isolated Dimensions', () => {
+    cy.get('.nav__link').contains('Isolated Dimensions').click();
+    cy.location('hash').should('eq', '#/data/#iso-dimensions');
+  });
+
+  it('2D Histograms', () => {
+    cy.get('.nav__link').contains('2D Histograms').click();
+    cy.location('hash').should('eq', '#/data/#histograms');
+  });
+
+  it('Correlation Matrices', () => {
+    cy.get('.nav__link').contains('Correlation Matrices').click();
+    cy.location('hash').should('eq', '#/data/#correlation');
   });
 });
